92921101fa2ea9718c54cfddc5857a5657d566d0
JiLoveZn <2031265447@qq.com>
2018-04-14 16:06:28 +0800

    后端框架

---
+++ buct_mini_program/src/main/java/com/miniprograms/buct_mini_program/BuctMiniProgramApplication.java
@@ -0,0 +1,12 @@
+package com.miniprograms.buct_mini_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuctMiniProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuctMiniProgramApplication.class, args);
+    }
+}
---
+++ buct_mini_program/src/main/java/com/miniprograms/buct_mini_program/handlerException/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.miniprograms.buct_mini_program.handlerException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
